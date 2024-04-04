b18498088f5c476292d0670aa6076b51c837ea42
mgtron278 <mgtron.278@gmail.com>
2024-04-04 14:39:42 -0400

    updated

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def get_conversational_chain():
     Answer:
     """
 
-    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.5)
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
     prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
     return chain
